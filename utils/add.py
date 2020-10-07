39d997809d81e3b8616130a4ef7dcd2cb1ef6fa5
Martijn Bentum <bentummartijn@gmail.com>
2020-10-07 09:22:42 +0200

    rewritten for loading articles/papers into database

--- utils/add.py
+++ utils/add.py
@@ -8,11 +8,65 @@ import os
 from vocabulary.models import Paper as p
 from vocabulary.models import Article as a
 
+directory = '/vol/tensusers/mbentum/hongerwinter/oai2linerec/hongerwinter/'
+
 def get_filenames_ddd():
-	return glob.glob('ddd-*-ocr')
+	return glob.glob(directory+'ddd-*-ocr')
 
 def get_filenames_kranten():
-	return glob.glob('kranten-*-ocr')
+	return glob.glob(directory+'kranten-*-ocr')
+
+
+def add_done_file(name):
+	with open(directory+'done_files','a') as fout:
+		fout.write(name +'\n')
+
+def get_done_files():
+	with open(directory+'done_files','r') as fin:
+		return [f for f in fin.read().split('\n')]
+
+
+def set_reserved_file(name):
+	with open(directory+'reserved_files','a') as fout:
+		fout.write(name +'\n')
+
+def get_reserved_files():
+	with open(directory+'reserved_files','r') as fin:
+		return [f for f in fin.read().split('\n')]
+
+
+def handle():
+	for f in get_filenames_kranten() + get_filenames_ddd():
+		done = get_done_files() 
+		if f in done:
+			print(f,'already done moving to next file')
+			continue
+		set_reserved_file(f)
+		print('making paper object:',f)
+		p = Papers(f)
+		print('loading files in database')
+		print('npapers:',p.npapers)
+		print('narticles:',p.narticles)
+		p.make()
+		print('paper integrity error:',p.paper_errors)
+		print('article integrity error:',p.article_errors)
+		print('article no text:',p.no_text)
+		print('-'*80)
+		print('')
+		log(p,f)
+		add_done_file(f)
+
+def log(p,f):
+	m = '\nmaking paper object:'+f+'\n'
+	m+='npapers:'+str(p.npapers)+'\n'
+	m+='narticles:'+str(p.narticles)+'\n'
+	m+='paper integrity error:'+str(p.paper_errors)+'\n'
+	m+='article integrity error:'+str(p.article_errors)+'\n'
+	m+='article no text:'+str(p.no_text)+'\n'
+	m+=f+'\n'
+	m+='-'*80+'\n'
+	with open(directory+'db_log','a') as fout:
+		fout.write(m+'\n')
 
 
 
@@ -25,6 +79,11 @@ class Papers:
 		self.papers = [Paper(tt) for tt in t.split('<paper>')[1:]]
 		self.id2paper = dict([[paper.identifier,paper] for paper in self.papers if hasattr(paper,'identifier')])
 		self.npapers = len(self.papers)
+		self.narticles= sum([int(p.narticles) for p in self.papers])
+
+		self.paper_errors =0
+		self.article_errors =0
+		self.no_text= 0
 
 	def __repr__(self):
 		return str(self.papers[0].dt.year)
@@ -40,7 +99,7 @@ class Papers:
 			paper.make()
 		self.paper_errors = sum([p.integrity_error for p in self.papers])
 		self.article_errors = sum([p.article_errors for p in self.papers])
-
+		self.no_text= sum([p.no_text for p in self.papers])
 
 
 class Paper:
@@ -93,6 +152,7 @@ class Paper:
 		self._make_paper()
 		self._make_db_articles()
 		self.article_errors = sum([a.integrity_error for a in self.articles])
+		self.no_text= sum([a.no_text for a in self.articles])
 
 
 class Article:
@@ -100,6 +160,7 @@ class Article:
 		self.xml = xml
 		self._set_info()
 		self.integrity_error = False
+		self.no_text = False
 
 	def __repr__(self):
 		return self.title 
@@ -114,6 +175,9 @@ class Article:
 			setattr(self,e.tag,e.text)
 
 	def _make_article(self,paper):
+		if self.text == None:
+			self.no_text = True
+			return
 		self.article = a(
 			a_paper = paper,
 			title = self.title,
@@ -124,6 +188,7 @@ class Article:
 			date = paper.date)
 		try: self.article.save()
 		except IntegrityError: self.integrity_error = True
+	
 
 	def make(self,paper):
 		self._make_article(paper)
