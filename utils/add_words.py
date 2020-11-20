fa65f62b09428c80e807d1e85a71cd1184ce21c8
Martijn Bentum <bentummartijn@gmail.com>
2020-11-20 11:17:59 +0100

    check whether hongerwinter is present, added some log functionality

--- utils/add_words.py
+++ utils/add_words.py
@@ -7,8 +7,28 @@ from vocabulary.models import WordArticleRelation as war
 import time
 import ucto
 
+directory = '/vol/tensusers/mbentum/hongerwinter/oai2linerec/hongerwinter/'
+
 tokenizer = ucto.Tokenizer("tokconfig-nld",lowercase = True,
 	sentencedetection=False,paragraphdetection=False)
+
+lnd = dict([line.split('\t') for line in open('location_names_dict').read().split('\n') if line])
+
+def add_done_paper(name,location_category= 'landelijk',hw = True):
+	hw = '_hongerwinter' if hw else '_no_hongerwinter'
+	with open(directory+'done_papers_'+location_category+hw,'a') as fout:
+		fout.write(name +'\n')
+
+def get_done_paper(location_category='landelijk', hw = True):
+	hw = '_hongerwinter' if hw else '_no_hongerwinter'
+	with open(directory+'done_papers_'+location_category+hw,'r') as fin:
+		return [f for f in fin.read().split('\n') if f]
+
+def add_location_category():
+	for p in Paper.objects.all():
+		p.location_category = lnd[p.location]
+		p.save()
+
 
 def save2db(item,excists=[],error=[],ok=[]):
 	try: item.save()
@@ -24,11 +44,123 @@ def add_lexicon(lexicon = []):
 	if lexicon == []:lexicon = get_lexicon()
 	excists,error,ok = [],[],[]
 	for word in lexicon:
-		w = Word(w_word = word)
+		w = Word(word = word)
 		save2db(w,excists,error,ok)
 	print('created words, already exists:',len(excists),'saved:',len(ok),'error:',len(error))
-	print(' '.join(lexicon))
+	print(' | '.join(lexicon))
+	return excists,error,ok
 
+
+
+def add_all_word_article_relations(lexicon=[], location_category='landelijk',check_hw = True):
+	'''add word article relations for articles in the database that are
+	published in a newspaper corresponding to the location category, 
+	for each word in the lexicon.
+	'''
+	words = get_words(lexicon,location_category)
+	if not words:
+		print('words done for category:',location_category,'\n',' '.join(lexicon))
+		return
+	excists,error,ok = [],[],[]
+	papers = Paper.objects.all()
+	npapers = len(papers)
+	start = time.time()
+	for i,x in enumerate(papers):
+		if location_category == 'all':print('handling all location_categories')
+		elif x.location_category != location_category: 
+			print('skipping paper with location category:',x.location_category)
+			continue
+		if x.link in get_done_paper(location_category,check_hw): 
+			print('skipping paper',x,'already done')
+			continue
+		date = x.date
+		articles = x.article_set.all()
+		print('starting with paper',x,'with ',len(articles),' from ',date)
+		if i != 0:print(eta(start,i,npapers))
+		for article in articles:
+			add_word_article_relations(words,article,date,excists,error,ok,check_hw)
+		add_done_paper(x.link,location_category,check_hw)
+		
+	set_category_done(words,location_category)
+	print('created words, already exists:',len(excists),'saved:',len(ok),'error:',len(error))
+	return excists,error,ok
+
+
+def check_word_occurence(word,article_text,td=None):
+	'''check whether hongerwinter and the target word occurs in the text.
+	returns the number of time the target word occurred in the text'''
+	if td == None:td = article2counter(article)
+	hw_count = td['hongerwinter'] if 'hongerwinter' in td.keys() else 0
+	if ' ' in word.word: count = article_text.count(word.word)
+	else: count = td[word.word] if word.word in td.keys() else 0
+	return count, hw_count
+			
+
+def add_word_article_relations(words,article,date,excists=[],error=[],ok=[],check_hw = True):
+	'''check for each word the number of times it occurs in the article
+	create wordArticleRelation object that stores word, article, date & count info.
+	'''
+	td = article2counter(article)
+	article_text = article.text.lower()
+	for word in words:
+		count,hongerwinter_count = check_word_occurence(word,article_text,td)
+		if check_hw and hongerwinter_count == 0: continue
+		if count == 0: continue
+		relation = war(article = article, word = word,count = count,date = date)
+		save2db(relation,excists,error,ok)
+		
+	
+def article2tokens(article):
+	tokenizer.process(article.text)
+	return  [str(token).lower() for token in tokenizer]
+	
+def article2counter(article):
+	tokens = article2tokens(article)
+	return Counter(tokens)
+
+
+def get_words(lexicon=[],location_category='landelijk'):
+	'''Save words in the lexicon to the database, load the words from the database
+	exclude the words have already been handle for the location category
+	return list of words that are not yet handled for the location_category
+	'''
+	if lexicon == []:lexicon = get_lexicon()
+	add_lexicon(lexicon)
+	temp= [Word.objects.get(word=word) for word in lexicon]
+	words = []
+	for word in temp:
+		if check_done(word,location_category): continue
+		else: words.append(word)
+	return words
+
+def check_done(word,location_category):
+	'''check whether a word has already been processed (all word article relation been added)
+	for the current location category
+	'''
+	if location_category == 'landelijk':return word.national
+	if location_category == 'regionaal':return word.regional
+	if location_category == 'koloniaal':return word.colonial
+	else: raise ValueError(location_category+
+		' unknown, should be landelijk, regionaal or koloniaal')
+		
+def set_category_done(words,location_category):
+	'''set category done for each word in words.
+	'''
+	if location_category == 'landelijk':attr='national'
+	elif location_category == 'regionaal':attr='regional'
+	elif location_category == 'koloniaal':attr='colonial'
+	else: raise ValueError(location_category+ 
+		' unknown, should be landelijk, regionaal or koloniaal')
+	for word in words:
+		setattr(word,attr,True)
+		save2db(words)
+	print('set',attr,'to true for the following words:\n',' '.join([w.word for w in words]))
+
+
+
+
+
+#-- time helper function, estimate time the function call is done
 def delta_time(start):
 	return time.time() - start
 
@@ -52,88 +184,4 @@ def eta(start,index,n):
 	n_togo = n - (index+1)
 	etas = n_togo*estimate_per_unit
 	return seconds2time(etas) + ' | index: '+str(index) + ' total: '+str(n)
-
-
-def add_all_word_article_relations(lexicon=[], location_category='landelijk'):
-	'''add word article relations for articles in the database that are
-	published in a newspaper corresponding to the location category, 
-	for each word in the lexicon.
-	'''
-	words = get_words(lexicon,location_category)
-	if not words:
-		print('words done for category:',location_category,'\n',' '.join(lexicon))
-		return
-	excists,error,ok = [],[],[]
-	papers = Paper.objects.all()
-	npapers = len(papers)
-	start = time.time()
-	for i,x in enumerate(papers):
-		date = x.date
-		articles = x.article_set.all()
-		print('starting with paper',x,'with ',len(articles),' from ',date)
-		if i != 0:print(eta(start,i,npapers))
-		for article in articles:
-			add_word_article_relations(words,article,date,excists,error,ok)
-		
-	set_category_done(words,location_category)
-	print('created words, already exists:',len(excists),'saved:',len(ok),'error:',len(error))
-	return excists,error,ok
-			
-
-def add_word_article_relations(words,article,date,excists=[],error=[],ok=[]):
-	'''check for each word the number of times it occurs in the article
-	create wordArticleRelation object that stores word, article, date & count info.
-	'''
-	td = article2counter(article)
-	for word in words:
-		w = word.w_word
-		if w not in td.keys():continue
-		relation = war(war_article = article, war_word = word,count = td[w],date = date)
-		save2db(relation)
-		
-	
-def article2tokens(article):
-	tokenizer.process(article.text)
-	return  [str(token).lower() for token in tokenizer]
-	
-def article2counter(article):
-	tokens = article2tokens(article)
-	return Counter(tokens)
-
-
-def get_words(lexicon,location_category):
-	'''Save words in the lexicon to the database, load the words from the database
-	exclude the words have already been handle for the location category
-	return list of words that are not yet handled for the location_category
-	'''
-	if lexicon == []:lexicon = get_lexicon()
-	add_lexicon(lexicon)
-	temp= [Word.objects.get(w_word=word) for word in lexicon]
-	words = []
-	for word in temp:
-		if check_done(word,location_category): continue
-		else: words.append(word)
-	return words
-
-def check_done(word,location_category):
-	'''check whether a word has already been processed (all word article relation been added)
-	for the current location strategy
-	'''
-	if location_category == 'landelijk':return word.national
-	if location_category == 'regionaal':return word.regional
-	if location_category == 'koloniaal':return word.colonial
-	else: raise ValueError(location_category+
-		' unknown, should be landelijk, regionaal or koloniaal')
-		
-def set_category_done(words,location_category):
-	'''set category done for each word in words.
-	'''
-	if location_category == 'landelijk':attr='national'
-	if location_category == 'regionaal':attr='regional'
-	if location_category == 'koloniaal':attr='colonial'
-	else: raise ValueError(location_category+ 
-		' unknown, should be landelijk, regionaal or koloniaal')
-	for word in words:
-		setattr(word,attr,True)
-	print('set',attr,'to true for the following words:\n',' '.join([w.w_word for w in words]))
 
