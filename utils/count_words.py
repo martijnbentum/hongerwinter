a94a3531fad7b24ee1f7a351885e4b2df35c4453
Martijn Bentum <bentummartijn@gmail.com>
2020-11-20 13:53:46 +0100

    an all word counter to check the number of words for a given date and newspaper category

--- utils/count_words.py
+++ utils/count_words.py
@@ -1,8 +1,57 @@
 from vocabulary.models import WordArticleRelation as war
 from vocabulary.models import Article,Paper,Word
+from .add_words import article2counter, eta
+import time
+
+articles = []
+
+class AllWordCounts:
+	'''create counts for each year for different categories of newspapers.'''
+	def __init__(self,filename = 'all_word_counts'):
+		global articles
+		print('init..')
+		self.filename = filename
+		if not articles:
+			self.articles = list(Article.objects.all())
+			articles = self.articles
+		else: self.articles = articles
+		self._make_counts()
+
+	def _make_counts(self):
+		self.regionaal, self.landelijk = {}, {}
+		for name in 'decade,year,month'.split(','):
+			self.regionaal[name],self.landelijk[name] = {}, {}
+		print('counting...')
+		narticles = len(self.articles)#32724636#len(self.articles)
+		start = time.time()
+		for i,article in enumerate(self.articles):
+			print(eta(start,i,narticles))
+			print(i,narticles,'\t',article)
+			c = article2counter(article)
+			if article.nword_types == None:
+				article.nword_tokens = sum(c.values())
+				article.nword_types = len(c.keys())
+				article.save()
+			self._fill_dicts(article,sum(c.values()))
+
+	def _fill_dicts(self,article,count):
+		lc = article.paper.location_category
+		if lc not in 'landelijk,regionaal'.split(','): return
+		date = article.date
+		d = getattr(self,lc)
+		t = make_decade(date.year)
+		if t not in d['decade'].keys(): d['decade'][t] = 0
+		if date.year not in d['year'].keys(): d['year'][date.year] = 0
+		if (date.year,date.month) not in d['month'].keys(): d['month'][(date.year,date.month)] = 0
+		d['decade'][t] += count
+		d['year'][date.year] += count
+		d['month'][(date.year,date.month)] += count
+
+
 
 
 class WordInfos:
+	'''create counts word words listed in the lexicon for each year.'''
 	def __init__(self,filename = 'word_counts_time'):
 		self.filename = filename
 
@@ -45,7 +94,7 @@ class WordInfo:
 		self.word_dict = make_word_dict(self.word)
 		decade,year,month =  {},{},{}
 		for key in self.word_dict.keys():
-			t = self._make_decade(key.year)
+			t = make_decade(key.year)
 			if t not in decade.keys(): decade[t] = 0
 			if key.year not in year.keys(): year[key.year] = 0
 			if (key.year,key.month) not in month.keys(): month[(key.year,key.month)] = 0
@@ -54,11 +103,11 @@ class WordInfo:
 			month[(key.year,key.month)] += self.word_dict[key]
 		self.decade,self.year,self.month = decade,year,month
 
-	def _make_decade(self,year):
-		s = str(year)
-		if int(s[-1]) < 5: return int(s[:3] + '0')
-		if s[2] == '9': return 2000
-		return int(s[:2] + str(int(s[2])+1) + '0')
+def make_decade(year):
+	s = str(year)
+	if int(s[-1]) < 5: return int(s[:3] + '0')
+	if s[2] == '9': return 2000
+	return int(s[:2] + str(int(s[2])+1) + '0')
 
 
 
